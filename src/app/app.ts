cbafe94666802ed6de2152d79ca0a2e334e4cfc7
patron290 <balikapatrik@gmail.com>
2025-12-11 19:43:18 +0100

    feat: Integrate Angular Material, implement new app layout with sidenav, and add associated assets and styling.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatListModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
